b84dfa58ec1f2a08492a39645be8896c3f22698f
joseph <staylordsxd@gmail.com>
2023-06-12 11:32:31 +0200

    first commit

---
+++ src/main/kotlin/com/staylords/launchers/LaunchersPlugin.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 Joseph (me@staylords.com)
+ *
+ * Use and or redistribution of compiled JAR file and or source code is permitted only if given
+ * explicit permission from original author: staylords
+ */
+  
+ package com.staylords.launchers
+
+/**
+ * @project hypixel_launchers_cb
+ *
+ * @date 12/06/2023
+ * @author me@staylords.com
+ */class LaunchersPlugin {
+}
